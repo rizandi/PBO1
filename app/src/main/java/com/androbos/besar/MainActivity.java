67d1d4b4448853fa713c0f4553876faa8339dcff
tommy <tommyputranto1@gmail.com>
2015-08-30 10:52:30 +0700

    tambah Toast

--- app/src/main/java/com/androbos/besar/MainActivity.java
+++ app/src/main/java/com/androbos/besar/MainActivity.java
@@ -1,19 +1,51 @@
 package com.androbos.besar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView angka;
+    private Button countBtn;
+    private Button pageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        angka = (TextView)findViewById(R.id.angka);
+        countBtn = (Button)findViewById(R.id.countBtn);
+        pageBtn=(Button)findViewById(R.id.pageBaru);
+        countBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Text", Toast.LENGTH_SHORT).show();
+                int angkaText = 1;
+
+                angka.setText(String.valueOf(angkaText));
+            }
+        });
+        pageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nama = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(nama);
+            }
+        });
+
 
 
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
